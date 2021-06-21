67c31f8a1fa6786f6219ec7db299ebb57d533697
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-06-21 23:19:26 +0530

    [Prajakta]: Added swagger and appconfiguration file

---
+++ src/main/java/com/bridgelabz/bookstore/configuration/AppConfiguration.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.bookstore.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//Model Mapper Class
+
+@Configuration
+public class AppConfiguration {
+
+	@Bean
+	public ModelMapper modelMapper() {
+	ModelMapper modelMapper = new ModelMapper();
+	modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+	return modelMapper;
+	}
+}
