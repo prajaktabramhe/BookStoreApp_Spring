4a8e0c8ce15fedadfa1dd8e8e1b2685a7e65f099
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-06-23 11:30:29 +0530

    [Prajakta]: Added Bussiness Logic for book api

--- src/main/java/com/bridgelabz/bookstore/service/IBookService.java
+++ src/main/java/com/bridgelabz/bookstore/service/IBookService.java
@@ -17,4 +17,13 @@ public interface IBookService {
 	// To update book details
 	Response updateBook(String token, BookDTO bookDto);
 
+	// To delete user
+	Response deleteBook(String token, String bookName);
+
+	// To update book price
+	Response updateBookPrice(String token, String bookName, Double price);
+
+	// To update book quantity
+	Response updateBookQuantity(String token, String bookName, int quantity);
+
 }
